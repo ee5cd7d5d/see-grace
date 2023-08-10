b31e8d0f2633d4f4206ee968aea2b92743e67ff9
romitow <romi9815.4@gmail.com>
2023-08-11 01:08:49 +0200

    To decode the bytes to legible info

--- trans.py
+++ trans.py
@@ -1,13 +1,50 @@
-'''Transform NASA data into binary'''
+"""Transform NASA data into binary"""
+import struct
 
-with open('GNV1B_2002-04-04_A_02.dat', 'rb', encoding=None) as input_file:
+with open("GNV1B_2002-04-04_A_02.dat", "rb", encoding=None) as input_file:
+    # To ascertain the position of the header
     lines = input_file.read()
-    res = []                    # empty array
-    for byte in lines:
-        binary_rep = bin(byte)  # convert to binary representation
-        res.append(binary_rep)  # add to list
-    #print(res)
+    NEWLINE_CHAR = 0x0A
+    header_width = lines.find(NEWLINE_CHAR)
+    print(header_width)
+    LINE_COUNT = 0
+    header_line = lines[0:header_width].decode("ascii")
+    print(header_line)
+    while not "END OF HEADER" in header_line:
+        header_line = lines[(LINE_COUNT * header_width) :
+                            ((LINE_COUNT + 1) * header_width)].decode("ascii")
+        print(header_line)
+        print(LINE_COUNT)
+        LINE_COUNT += 1
+    print(f"{(header_width * (LINE_COUNT + 1))} bytes in header")
 
-with open('convertedres.dat','x', encoding='UTF-8') as create_file:
-    for line in res:
-        create_file.write(" ".join(line) + "\n")
+    # To read the information to legible format
+    IN_POS = header_width * (LINE_COUNT + 1)
+    # Type of info to decode
+    typeInfo = ["int", "chr", "chr", "dp", "dp", "dp", "dp",
+            "dp", "dp", "dp", "dp", "dp", "dp", "dp", "dp",
+            "uchar"]
+    # Order of byte groups to decode
+    bytePack = [4, 1, 1, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1]
+    packTotal = sum(bytePack)  #Total length of pack. in bytes
+    PACK_LENGTH = len(bytePack)
+    if len(bytePack) == len(typeInfo):  # For debugging purposes
+        print(True)
+    decodPack = []
+    leftoverBytes = (len(lines)- IN_POS)%packTotal
+    print(leftoverBytes)        # To check if there are leftover bytes
+    IN_POS += leftoverBytes     # So the initial position is adequate to finish in a block
+    while IN_POS < len(lines):  # Iterate through the read file
+        IC = 0
+        for byte in bytePack:   # Iterate through one data pack
+            if typeInfo[IC] == "int":
+                decodPack.append(struct.unpack('>I',lines[IN_POS:(IN_POS+byte)]))
+            if typeInfo[IC] == "chr":
+                decodPack.append((lines[IN_POS:(IN_POS+byte)]).decode("ascii"))
+            if typeInfo[IC] == "dp":
+                decodPack.append(struct.unpack('>d',lines[IN_POS:(IN_POS+byte)]))
+            if typeInfo[IC] == "uchar":
+                decodPack.append((lines[IN_POS:(IN_POS+byte)].decode("ascii")))
+            IN_POS += byte
+            IC += 1
+    print(decodPack)
