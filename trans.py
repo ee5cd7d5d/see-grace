f909df8a368f8e62595fa3f97b1deedf7993d079
romitow <romi9815.4@gmail.com>
2023-08-12 15:24:57 +0200

    Some things fixed

--- trans.py
+++ trans.py
@@ -1,50 +1,70 @@
 """Transform NASA data into binary"""
 import struct
+import logging
 
+def find_header(counter, width):
+    '''To get the position of END OF HEADER'''
+    header_line = lines[0:width].decode("ascii")
+    for i in range(0,len(lines)):
+        header_line = lines[(i * width) :
+                            ((i + 1) * width)].decode("ascii")
+        logging.info(i)
+        logging.info(header_line)
+        if "END OF HEADER" in header_line:
+            counter = i
+            return width * (counter + 1), counter
+    return -1, -1
+
+
+logging.basicConfig(filename='example.log', encoding='utf-8',filemode='w', level=logging.DEBUG)
 with open("GNV1B_2002-04-04_A_02.dat", "rb", encoding=None) as input_file:
     # To ascertain the position of the header
     lines = input_file.read()
-    NEWLINE_CHAR = 0x0A
+    NEWLINE_CHAR = 0x0A  # from HEX reader for the file GNV
     header_width = lines.find(NEWLINE_CHAR)
-    print(header_width)
+    logging.info('Header width is %s', header_width)
     LINE_COUNT = 0
-    header_line = lines[0:header_width].decode("ascii")
-    print(header_line)
-    while not "END OF HEADER" in header_line:
-        header_line = lines[(LINE_COUNT * header_width) :
-                            ((LINE_COUNT + 1) * header_width)].decode("ascii")
-        print(header_line)
-        print(LINE_COUNT)
-        LINE_COUNT += 1
-    print(f"{(header_width * (LINE_COUNT + 1))} bytes in header")
+    firstline = lines[0:header_width].decode("ascii")
+    print(firstline)
+    IN_POS, LINE_COUNT = find_header(LINE_COUNT, header_width)
+    logging.info("Position of last line, %s", LINE_COUNT)
+    logging.info("%s bytes in header",IN_POS)
+    if IN_POS == -1 or LINE_COUNT == -1:
+        logging.error("End of header not found")
 
     # To read the information to legible format
-    IN_POS = header_width * (LINE_COUNT + 1)
-    # Type of info to decode
-    typeInfo = ["int", "chr", "chr", "dp", "dp", "dp", "dp",
+    logging.info("Initial position, %s",IN_POS)
+    typeInfo = ["int", "chr", "chr", "dp", "dp", "dp", "dp",     # Type of info to decode
             "dp", "dp", "dp", "dp", "dp", "dp", "dp", "dp",
             "uchar"]
-    # Order of byte groups to decode
-    bytePack = [4, 1, 1, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1]
+    bytePack = [4, 1, 1, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 1]  # Order of byte groups to decode
     packTotal = sum(bytePack)  #Total length of pack. in bytes
+    logging.info("The length of a pack. is %s",packTotal)
     PACK_LENGTH = len(bytePack)
-    if len(bytePack) == len(typeInfo):  # For debugging purposes
-        print(True)
+    # For debugging purposes
+    if len(bytePack) == len(typeInfo):
+        logging.info('There is an information type for each set of bytes')
+    else:
+        logging.warning('There are leftover packs of data without a type of information')
     decodPack = []
+    # To check if there are leftover bytes
     leftoverBytes = (len(lines)- IN_POS)%packTotal
-    print(leftoverBytes)        # To check if there are leftover bytes
-    IN_POS += leftoverBytes     # So the initial position is adequate to finish in a block
+    logging.warning('There are %s leftover bytes',leftoverBytes)
+    if leftoverBytes:
+        IN_POS += leftoverBytes     # So the initial position is adequate to finish in a block
+    logging.warning('The initial position has been switched to %s', IN_POS)
+    # To interpret the information
     while IN_POS < len(lines):  # Iterate through the read file
         IC = 0
         for byte in bytePack:   # Iterate through one data pack
             if typeInfo[IC] == "int":
                 decodPack.append(struct.unpack('>I',lines[IN_POS:(IN_POS+byte)]))
-            if typeInfo[IC] == "chr":
+            elif typeInfo[IC] == "chr":
                 decodPack.append((lines[IN_POS:(IN_POS+byte)]).decode("ascii"))
-            if typeInfo[IC] == "dp":
+            elif typeInfo[IC] == "dp":
                 decodPack.append(struct.unpack('>d',lines[IN_POS:(IN_POS+byte)]))
-            if typeInfo[IC] == "uchar":
+            elif typeInfo[IC] == "uchar":
                 decodPack.append((lines[IN_POS:(IN_POS+byte)].decode("ascii")))
             IN_POS += byte
             IC += 1
-    print(decodPack)
+    print(decodPack[0:160])
