8b5e72d8ac0835827c54476adb5041070589a2a7
romitow <romi9815.4@gmail.com>
2023-08-15 21:35:16 +0200

    Binconv (#3)
    * Cambio de prueba jeje
    
    * ya he cambiado las cosas malo :(
    
    * v1
    
    v2

--- trans.py
+++ trans.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Transform NASA data into binary"""
 import struct
 import logging
@@ -94,18 +93,4 @@ with open("GNV1B_2002-04-04_A_02.dat", "rb", encoding=None) as input_file:
             IN_POS += byte
             IC += 1
     print(decodPack[0:160])
-=======
-'''Transform NASA data into binary'''
 
-with open('GNV1B_2002-04-04_A_02.dat', 'rb', encoding=None) as input_file:
-    lines = input_file.read()
-    res = []                    # empty array
-    for byte in lines:
-        binary_rep = bin(byte)  # convert to binary representation
-        res.append(binary_rep)  # add to list
-    #print(res)
-
-with open('convertedres.dat','x', encoding='UTF-8') as create_file:
-    for line in res:
-        create_file.write(" ".join(line) + "\n")
->>>>>>> 9b1353e (Binconv (#3))
